ea28b8c5c16afb7ed833beb45643d23691ef56fd
GU1LL0T1NE <code_kwen@outlook.com>
2025-07-22 22:08:17 -0500

    Add mock data and search bar component

---
+++ spotify-api-project/src/SearchBar.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import Data from './mockData.json'
+export default function SearchBar() {
+    return (
+        <div>
+            <input type='' placeholder='Search for your favorite songs' />
+        </div>
+    )
+}
