5506ce69c5268c4d561c4c2d2b1525ab5961d815
GU1LL0T1NE <code_kwen@outlook.com>
2025-07-23 22:34:20 -0500

    Add additional styling

--- spotify-api-project/src/SearchResults.jsx
+++ spotify-api-project/src/SearchResults.jsx
@@ -4,15 +4,15 @@ import styles from './stylesheets/searchResultsStyle.module.css';
 
 function SearchResults() {
     return (
-        <div>
+        <>
             {Data.map(songs => (
-                <div className={styles.resultsContainer}>
+                <div key={songs['song-title']} className={styles.resultsContainer}>
                     <h1>{songs['song-title']}</h1>
                     <h2>Artist: {songs.artist}</h2>
                     <p>Genre: {songs.genre}</p>
                 </div>
             ))}
-        </div>
+        </>
     )
 }
 
